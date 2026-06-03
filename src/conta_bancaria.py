a9ea651e687c674aeb5553a6444833271f15aa21
Kauann Mendes <kauannkkf@gmail.com>
2026-06-02 23:36:04 -0300

    Adição dos métodos no conta_bancaria.py

--- src/conta_bancaria.py
+++ src/conta_bancaria.py
@@ -1,1 +1,29 @@
+class ContaBancaria:
 
+    def __init__(self,
+                 numero,
+                 titular,
+                 banco,
+                 saldo_inicial):
+        pass
+
+    def depositar(self, valor):
+    pass
+    
+    def sacar(self, valor):
+        pass
+    
+    def consultar_saldo(self):
+        pass
+    
+    def get_numero(self):
+        pass
+    
+    def get_titular(self):
+        pass
+    
+    def get_banco(self):
+        pass
+    
+    def esta_ativa(self):
+        pass
