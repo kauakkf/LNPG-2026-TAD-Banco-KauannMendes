b5c166f3d28a5fe33d6f3553fffc8c3797db7cfa
Kauann Mendes <kauannkkf@gmail.com>
2026-06-02 23:32:36 -0300

    Funções da validador.py

--- src/validador.py
+++ src/validador.py
@@ -1,1 +1,17 @@
+class Validador:
 
+    @staticmethod
+    def nome_valido(nome):
+        pass
+
+    @staticmethod
+    def cpf_valido(cpf):
+        pass
+
+    @staticmethod
+    def telefone_valido(telefone):
+        pass
+
+    @staticmethod
+    def email_valido(email):
+        pass
