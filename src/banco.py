88ff1c7648ed383c65fa52167816935f1ec4aef6
Kauann Mendes <kauannkkf@gmail.com>
2026-06-02 23:37:47 -0300

    Adição dos métodos de gerenciamento no banco.py

--- src/banco.py
+++ src/banco.py
@@ -1,1 +1,20 @@
+class Banco:
 
+    def __init__(self, nome, codigo):
+
+        self.contas = []
+
+    def adicionar_conta(self, conta):
+        pass
+
+    def remover_conta(self, numero):
+        pass
+    
+    def buscar_conta(self, numero):
+        pass
+    
+    def quantidade_contas(self):
+        pass
+    
+    def listar_contas(self):
+        pass
