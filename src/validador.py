6243d65175c297e94ad5f215faf1cfed9fcf182b
Kauann Mendes <kauannkkf@gmail.com>
2026-06-02 23:48:57 -0300

    Implementação dos validadores no validador.py

--- src/validador.py
+++ src/validador.py
@@ -2,16 +2,32 @@ class Validador:
 
     @staticmethod
     def nome_valido(nome):
-        pass
+        return nome is not None and len(nome.strip()) >= 5
 
     @staticmethod
     def cpf_valido(cpf):
-        pass
+        
+        if cpf is None:
+            return False
+
+        cpf = ''.join(filter(str.isdigit, cpf))
+
+        return len(cpf) == 11
 
     @staticmethod
     def telefone_valido(telefone):
-        pass
+        
+        if telefone is None:
+            return False
+
+        telefone = ''.join(filter(str.isdigit, telefone))
+
+        return len(telefone) in (10, 11)
 
     @staticmethod
     def email_valido(email):
-        pass
+        
+        if email is None:
+            return False
+
+        return '@' in email
