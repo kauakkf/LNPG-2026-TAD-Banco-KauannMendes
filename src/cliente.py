3f0a73244a1c0b3c0c09acd0571e34b77b566cf7
Kauann Mendes <kauannkkf@gmail.com>
2026-06-02 23:34:46 -0300

    Adição dos métodos no cliente.py

--- src/cliente.py
+++ src/cliente.py
@@ -1,1 +1,25 @@
+class Cliente:
 
+    def __init__(self, nome, cpf,
+                 telefone=None,
+                 email=None):
+                   
+        pass
+    
+    def get_nome(self):
+        pass
+    
+    def get_cpf(self):
+        pass
+    
+    def get_telefone(self):
+        pass
+    
+    def get_email(self):
+        pass
+    
+    def alterar_telefone(self, telefone):
+        pass
+    
+    def alterar_email(self, email):
+        pass
